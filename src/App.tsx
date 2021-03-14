bcdc04191c754857fbecfe0667616eb59d0d076f
C-STYR <colestyron@gmail.com>
2021-03-14 09:10:46 -0800

    wadding message

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css'
 
 const colors: string[] = ["#D18DF0", " #F6E183", " #BDE666", " #FF555D", " #77A7FF"]
 
@@ -9,18 +10,27 @@ const key = (): number => Math.random();
 const colorKey = (): number => Math.floor(Math.random()*5)
 
 const sous: JSX.Element[] = paves.map(e => {
-  if (e === " ") return <div className="" style={{border: "1px solid #001629"}} key={key()}>{e}</div>;
-  else return <div className="" key={key()} style={{border: `1px solid ${colors[colorKey()]}`}}>{e}</div>
+  if (e === " ") return <div className="empty" style={{border: "1px solid #001629"}} key={key()}>{e}</div>;
+  else return <div className="ltr" key={key()} style={{border: `2px solid ${colors[colorKey()]}`}}>{e}</div>
 })
 const la: JSX.Element[] = plage.map(e => {
-  if (e === " ") return <div className="" style={{border: "1px solid #001629"}} key={key()}>{e}</div>;
-  else return <div className="" key={key()} style={{border: `1px solid ${colors[colorKey()]}`}}>{e}</div>
+  if (e === " ") return <div className="empty" style={{border: "1px solid #001629"}} key={key()}>{e}</div>;
+  else return <div className="ltr" key={key()} style={{border: `2px solid ${colors[colorKey()]}`}}>{e}</div>
 })
 
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div>Hello World!</div>
-  );
+    <div style={{marginTop: "80px"}}>
+      <h2>Hello World!</h2>
+      <div style={{marginTop: "80px"}} className="message">
+        {sous}
+      </div>
+      <div className="message">
+        {la}
+      </div>
+    </div>
+  )
 }
+
 export default App;
