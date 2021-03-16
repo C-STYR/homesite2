131a9e53cdd5d6c6f4bde3c96d0190ee6a130fb7
C-STYR <colestyron@gmail.com>
2021-03-16 13:56:36 -0800

    updated links

--- src/intro.tsx
+++ src/intro.tsx
@@ -9,9 +9,12 @@ const intro: JSX.Element =
     <br/>
     <p>
       I work at <a href="reactime.io" style={{color: "#FF555D"}}>Reactime.io</a>. 
-      You can see samples of code I've written <a href="https://github.com/open-source-labs/reactime" >here</a>,   
-      <a href="https://github.com/HelpDesk-Technologies/HelpDesk-Pro"> here </a>
-      and <a href="https://github.com/Quiz-Whiz/Quiz-Whiz-App">here</a>.
+      You can see samples of code I've written <a href="https://github.com/open-source-labs/reactime" 
+      target="_blank" rel="noopener noreferrer">here</a>,   
+      <a href="https://github.com/HelpDesk-Technologies/HelpDesk-Pro" 
+      target="_blank" rel="noopener noreferrer"> here </a>
+      and <a href="https://github.com/Quiz-Whiz/Quiz-Whiz-App" 
+      target="_blank" rel="noopener noreferrer">here</a>.
     </p>
     <br />
     <p>
@@ -23,12 +26,16 @@ const intro: JSX.Element =
     <p>
       In my spare time, I build <a 
         href="https://www.flickr.com/photos/colestyron/albums/72157718666104172"
+        target="_blank" rel="noopener noreferrer"
         style={{color: "#77A7FF"}}>wooden boats</a> and <a 
         href="https://www.flickr.com/photos/colestyron/albums/72157718672994412"
         style={{color: "#D18DF0"}}
+        target="_blank" rel="noopener noreferrer"
         >other things</a>. 
         I like long distance running 
-      and <a href="https://www.flickr.com/photos/colestyron/albums/72157718663723918" >skiing </a>depending on the season. 
+      and <a href="https://www.flickr.com/photos/colestyron/albums/72157718663723918" 
+      target="_blank" rel="noopener noreferrer"
+      >skiing </a>depending on the season. 
     </p>
   </div>
 
